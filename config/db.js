a843cfeced0989be850fba0f0c533b7e39c00585
jrsHenrique <jrs.henrique9@gmail.com>
2024-11-14 22:25:40 -0300

    Creating server and starting urm

--- config/db.js
+++ config/db.js
@@ -1,26 +1,35 @@
 // config/db.js
+const { trusted } = require('mongoose');
 const sql = require('mssql');
 require('dotenv').config();
+
 
 const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   server: process.env.DB_SERVER,
   database: process.env.DB_DATABASE,
+  port: 1434,
   options: {
     encrypt: true, // Use SSL
-    enableArithAbort: true
+    enableArithAbort: true,
+    trustedConnection: true,
+    trustServerCertificate: true,
   }
 };
 
-const connectDB = async () => {
+const connectDB = () => {
   try {
-    await sql.connect(dbConfig);
+    sql.connect(dbConfig);
     console.log('Conectado ao SQL Server');
   } catch (error) {
     console.error('Erro ao conectar ao SQL Server:', error);
     process.exit(1);
   }
 };
+
+console.log('Conectando ao SQL Server...');
+
+connectDB();
 
 module.exports = { sql, connectDB };
